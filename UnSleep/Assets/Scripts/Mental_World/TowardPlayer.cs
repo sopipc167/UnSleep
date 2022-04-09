26eab13942c00fb4b242162dae2a84a917550bfb
Ohmin Kwon <ohminkwon2718@gmail.com>
2022-04-10 03:59:57 +0900

    정신세계 추가
    하늘

---
+++ UnSleep/Assets/Scripts/Mental_World/TowardPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowardPlayer : MonoBehaviour
+{
+    [Header("참조")]
+    public Transform playerPos;
+
+    private Vector3 basisPos;
+    private Vector3 dirToTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        basisPos = transform.position;
+        basisPos.y = playerPos.position.y;
+
+        dirToTarget = playerPos.position - basisPos;
+        transform.rotation = Quaternion.LookRotation(dirToTarget, Vector3.up);
+    }
+}
