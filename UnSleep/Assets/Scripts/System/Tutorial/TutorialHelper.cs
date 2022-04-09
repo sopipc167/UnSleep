d2c687f75c22b9684c3b52d369b3ff0f5339bcec
Ohmin Kwon <ohminkwon2718@gmail.com>
2022-04-10 04:03:07 +0900

    Revert "진행 시 이상한 것 수정"
    This reverts commit 6e98754b9617f302356b907cf301ff3bc7262aa6.

--- UnSleep/Assets/Scripts/System/Tutorial/TutorialHelper.cs
+++ UnSleep/Assets/Scripts/System/Tutorial/TutorialHelper.cs
@@ -11,6 +11,11 @@ public class TutorialInfo
 
 public class TutorialHelper : MonoBehaviour
 {
+    //임시
+    [Header("임시 - 테스트용")]
+    public bool showAtStart;
+    public int maxSize;
+
     [Header("퍼즐의 정보를 제한할 개수")]
     public TutorialInfo[] info;
     private TutorialInfo currentInfo;
@@ -22,14 +27,30 @@ public class TutorialHelper : MonoBehaviour
         puzzle = GetComponent<PuzzleTutorial>();
     }
 
+    // DUMMY
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            puzzle.SetTutorial(maxSize, 1);
+        }
+    }
+
     void Start()
     {
+        // DUMMY
+        if (showAtStart)
+        {
+            puzzle.SetTutorial(maxSize, 1);
+            return;
+        }
+
         for (int i = info.Length - 1; i >= 0; --i)
         {
             if (Dialogue_Proceeder.instance.CurrentEpiID >= info[i].id)
             {
                 currentInfo = info[i];
-                puzzle.SetTutorial(currentInfo.maxInfo, (currentInfo.maxInfo + 2) / 3);
+                puzzle.SetTutorial(currentInfo.maxInfo, (currentInfo.maxInfo - 1) / 3);
                 break;
             }
         }
