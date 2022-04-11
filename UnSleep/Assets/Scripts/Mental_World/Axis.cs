8dc5539474332753bbfa18175c87e7aa35e1d3e5
Ohmin Kwon <ohminkwon2718@gmail.com>
2022-04-11 12:43:19 +0900

    정신세계 코드 정리해봄
    문제 시 revert 해도 됨

--- UnSleep/Assets/Scripts/Mental_World/Axis.cs
+++ UnSleep/Assets/Scripts/Mental_World/Axis.cs
@@ -4,72 +4,46 @@ using UnityEngine;
 
 public class Axis : MonoBehaviour
 {
-    public Quaternion TargetRotation;
-    public Transform CameraVector;
-    public Transform player;
-    private Behaviour behaviour;
-    private Control control;
+    [Header("참조")]
+    public Control player;
 
-    public float RotationSpeed;
-    public float ZoomSpeed;
-    public float Distance;
+    [Header("카메라 회전 설정")]
+    public float distance;
+    public float rotationSpeed;
 
-    private Vector3 AxisVec;
-    private Vector3 Gap;
+    private Quaternion targetRotation;
+    private Quaternion playerRotation;
+    private Vector3 axisVec;
+    private float gapX;
+    private float gapY;
 
-    private Transform MainCamera;
-
-    void Start()
+    void LateUpdate()
     {
-        MainCamera = Camera.main.transform;
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        behaviour = player.GetComponent<Behaviour>();
-        control = player.GetComponent<Control>();
+        // 카메라 거리 유지 : Distance만큼 재우미랑 거리 유지
+        axisVec = player.transform.position;
+        axisVec += -transform.forward * distance;
+        transform.position = axisVec;
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        DisCamera();
-        CameraRoatation();
-
-    }
-
-    void DisCamera() //카메라 거리 유지 : Distance만큼 재우미랑 거리 유지
-    {
-        AxisVec = transform.forward * -1; // * -1 은 왜하는거지? 
-        AxisVec *= Distance;
-        MainCamera.position = transform.position + AxisVec;
-    }
-
-    void CameraRoatation()
-    {
-        if (transform.rotation != TargetRotation)
-            transform.rotation = Quaternion.Slerp(transform.rotation, TargetRotation, RotationSpeed * Time.deltaTime);
-
+        // 마우스 우클릭 후 좌우로 움직이면 카메라도 움직임
+        // 단, 움직이는 중에는 회전 불가
         if (Input.GetMouseButton(1))
         {
-            // 값을 축적.
-            Gap.x += Input.GetAxis("Mouse Y") * RotationSpeed * -1;
-            Gap.y += Input.GetAxis("Mouse X") * RotationSpeed;
+            // 값을 축적
+            gapY += Input.GetAxis("Mouse X") * rotationSpeed;
+            gapX += Input.GetAxis("Mouse Y") * -rotationSpeed;
 
-            // 카메라 회전범위 제한.
-            Gap.x = Mathf.Clamp(Gap.x, -5f, 85f);
-            // 회전 값을 변수에 저장.
-            TargetRotation = Quaternion.Euler(Gap);
+            // 카메라 X축 회전범위 제한
+            gapX = Mathf.Clamp(gapX, -20f, 65f);
 
-            // 카메라벡터 객체에 Axis객체의 x,z회전 값을 제외한 y값만을 넘긴다.
-            Quaternion q = TargetRotation;
-            q.x = q.z = 0;
-            CameraVector.transform.rotation = q;
+            // 회전 값을 변수에 저장
+            targetRotation = Quaternion.Euler(new Vector3(gapX, gapY, 0f));
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
-            if (!behaviour.Run(control.targetPos))
-            {
-                player.transform.rotation = q;
-
-            }
-
+            // 카메라가 움직이면 잠재우미도 회전한다.
+            playerRotation = targetRotation;
+            playerRotation.x = 0f;
+            playerRotation.z = 0f;
+            player.transform.rotation = playerRotation;
         }
     }
 }
--- UnSleep/Assets/Scripts/Mental_World/Behaviour.cs
+++ UnSleep/Assets/Scripts/Mental_World/Behaviour.cs
@@ -4,40 +4,4 @@ using UnityEngine;
 
 public class Behaviour : MonoBehaviour
 {
-    public float speed = 2.0f; //이동속도
-    public float TurnSpeed = 10.0f; //시점 회전 속도
-    private Rigidbody rigid;
-    private Animator animator;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigid = GetComponent<Rigidbody>();
-        animator = GetComponentInChildren<Animator>();
-    }
-
-    public bool Run(Vector3 targetPos) //targetPos 좌표로 이동
-    {
-        animator.SetBool("Running", true);
-        //지금 위치랑 이동할 위치 사이 거리 
-        float dis = Vector3.Distance(transform.position, targetPos);
-        if (dis >= 0.01f) //어느정도 차이가 있으면 이동
-        {
-            transform.localPosition = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime); //스크립트가 달린 오브젝트가 이동
-
-            return true; //이동했으면 true 
-        }
-        animator.SetBool("Running", false);
-        return false; //이동 안했으면 false
-    }
-
-    public void Turn(Vector3 targetPos) //targetPos로 잠재우미 회전 *자연스럽게 수정하면 참 좋을듯
-    {
-        Vector3 dir = targetPos - transform.position; //방향벡터 = 클릭 좌표 - 지금 좌표 
-        //Debug.Log(dir);
-        Vector3 dirXZ = new Vector3(dir.x, 0f, dir.z); //위아래로 팔랑거리지는 않으니까 y좌표는 0
-
-        Quaternion targetRot = Quaternion.LookRotation(dirXZ); //y가 0인 벡터3 dirXZ로 회전
-        rigid.transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, TurnSpeed * Time.deltaTime); //from이 tar을 TurnSpeed로 돌아보도록
-    }
 }
