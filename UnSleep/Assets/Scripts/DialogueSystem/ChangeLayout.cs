42e3cb5a86a7dfe7440faf8514611b970d2646c8
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-04-29 21:55:15 +0900

    로그 수정중
    알맞은 배경/일러스트를 거슬러 찾을 수 없는 문제

--- UnSleep/Assets/Scripts/DialogueSystem/ChangeLayout.cs
+++ UnSleep/Assets/Scripts/DialogueSystem/ChangeLayout.cs
@@ -13,22 +13,37 @@ public class ChangeLayout : MonoBehaviour
     public RectTransform speaker2;
 
 
+    private int LastLayout;
+
 
     public void LayoutChange(int layoutnum)
     {
+        if (LastLayout == layoutnum) //바꾸려는 레이아웃이 현재 레이아웃과 같다면 그만
+            return;
+
         if (layoutnum == 1)
         {
             LINE.DOAnchorPos(new Vector2(0, 0), 1);
             NAME.DOAnchorPos(new Vector2(0, -12), 1);
             speaker1.DOAnchorPos(new Vector2(-460, -45), 1);
             speaker2.DOAnchorPos(new Vector2(460, -45), 1);
+            LastLayout = 1;
         }
-        else if (layoutnum == 2 || layoutnum ==3)
+        else if (layoutnum == 2)
         {
             LINE.DOAnchorPos(new Vector2(0, -581), 1);
             NAME.DOAnchorPos(new Vector2(0, -593), 1);
             speaker1.DOAnchorPos(new Vector2(-460, -1043), 1);
             speaker2.DOAnchorPos(new Vector2(460, -1043), 1);
+            LastLayout = 2;
+        }
+        else if (layoutnum == 3)
+        {
+            LINE.DOAnchorPos(new Vector2(0, -581), 1);
+            NAME.DOAnchorPos(new Vector2(0, -593), 1);
+            speaker1.DOAnchorPos(new Vector2(-460, -1043), 1);
+            speaker2.DOAnchorPos(new Vector2(460, -1043), 1);
+            LastLayout = 3;
         }
         else if (layoutnum == 4)
         {
@@ -36,6 +51,7 @@ public class ChangeLayout : MonoBehaviour
             NAME.DOAnchorPos(new Vector2(0, -200), 1);
             speaker1.DOAnchorPos(new Vector2(-460, -1043), 1);
             speaker2.DOAnchorPos(new Vector2(460, -1043), 1);
+            LastLayout = 4;
         }
     }
 
