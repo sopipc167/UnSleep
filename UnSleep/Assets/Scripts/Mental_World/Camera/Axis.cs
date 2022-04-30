cd4739e530a7e6d3c9632ebccd82ca19fffd04a9
Ohmin Kwon <ohminkwon2718@gmail.com>
2022-04-30 22:50:25 +0900

    하늘 삭제 및 움직임 수정
    하늘 에셋 자체를 삭제
    카메라 회전 오류 해결
    이동 코드 미미한 최적화
    폴더 정리 (일부 코드 충돌 우려 때문에 안 올림)

---
+++ UnSleep/Assets/Scripts/Mental_World/Camera/Axis.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis : MonoBehaviour
+{
+    [Header("참조")]
+    public Transform playerPos;
+
+    [Header("카메라 회전 설정")]
+    public float distance;
+    public float rotationSpeed;
+
+    private Quaternion targetRotation;
+    private Quaternion playerRotation;
+    private Vector3 axisVec;
+    private float gapX;
+    private float gapY;
+
+    void LateUpdate()
+    {
+        // 카메라 거리 유지 : Distance만큼 재우미랑 거리 유지
+        axisVec = playerPos.position;
+        axisVec += -transform.forward * distance;
+        transform.position = axisVec;
+
+        // 마우스 우클릭 후 좌우로 움직이면 카메라도 움직임
+        if (Input.GetMouseButton(1))
+        {
+            // 값을 축적
+            gapY += Input.GetAxis("Mouse X") * rotationSpeed;
+            gapX += Input.GetAxis("Mouse Y") * -rotationSpeed;
+
+            // 카메라 X축 회전범위 제한
+            gapX = Mathf.Clamp(gapX, -20f, 65f);
+
+            // 회전 값을 변수에 저장
+            targetRotation = Quaternion.Euler(new Vector3(gapX, gapY, 0f));
+            playerRotation = targetRotation;
+            playerRotation.x = 0f;
+            playerRotation.z = 0f;
+        }
+
+        // 카메라 회전 & 잠재우미 회전
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        playerPos.rotation = playerRotation;
+    }
+}
---
+++ UnSleep/Assets/Scripts/Mental_World/Camera/CameraBG.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBG : MonoBehaviour
+{
+    Color babyyellow = new Color(242f/255f, 243f/255f, 228f/255f);
+    Color coral = new Color(255f/255f, 216f/255f, 216f/255f);
+    Color skyblue = new Color(235f/255f, 255f/255f, 249f/255f);
+    Color pastelgreen = new Color(238f/255f, 253f/255f, 230f/255f);
+    Color lavender = new Color(245f/255f, 230f/255f, 253f/255f);
+    public float duration = 5.0f;
+   
+    void Start()
+    {
+        Camera.main.clearFlags = CameraClearFlags.SolidColor;
+        StartCoroutine("LC");
+    }
+
+
+    IEnumerator LC()
+    {
+        float t=0f;
+        while (t/duration < 1f)
+        {
+            Camera.main.backgroundColor = Color.Lerp(lavender, coral, t/duration);
+            t += 0.1f;
+            yield return new WaitForSeconds(.1f);
+        }
+        yield return StartCoroutine("CY");
+    }
+
+    IEnumerator CY()
+    {
+        float t = 0f;
+        while (t/duration < 1f)
+        {
+            Camera.main.backgroundColor = Color.Lerp(coral, babyyellow, t/duration);
+            t += 0.1f;
+            yield return new WaitForSeconds(.1f);
+        }
+        yield return StartCoroutine("YG");
+    }
+
+    IEnumerator YG()
+    {
+        float t = 0f;
+        while (t / duration < 1f)
+        {
+            Camera.main.backgroundColor = Color.Lerp(babyyellow, pastelgreen, t / duration);
+            t += 0.1f;
+            yield return new WaitForSeconds(.1f);
+        }
+        yield return StartCoroutine("GB");
+    }
+
+    IEnumerator GB()
+    {
+        float t = 0f;
+        while (t / duration < 1f)
+        {
+            Camera.main.backgroundColor = Color.Lerp(pastelgreen,skyblue, t / duration);
+            t += 0.1f;
+            yield return new WaitForSeconds(.1f);
+        }
+        yield return StartCoroutine("BL");
+    }
+
+    IEnumerator BL()
+    {
+        float t = 0f;
+        while (t / duration < 1f)
+        {
+            Camera.main.backgroundColor = Color.Lerp(skyblue, lavender, t / duration);
+            t += 0.1f;
+            yield return new WaitForSeconds(.1f);
+        }
+        yield return StartCoroutine("LC");
+    }
+
+
+
+}
