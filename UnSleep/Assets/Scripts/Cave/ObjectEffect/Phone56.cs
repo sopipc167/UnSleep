de08fcc153ae19edf25b833c3feabe53ee86a4dc
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-05-14 20:00:22 +0900

    56세 동굴 오브젝트 (일단 완)

--- UnSleep/Assets/Scripts/Cave/ObjectEffect/Phone56.cs
+++ UnSleep/Assets/Scripts/Cave/ObjectEffect/Phone56.cs
@@ -8,26 +8,39 @@ public class Phone56 : MonoBehaviour
     public string[] msgs;
     public Text text;
     public GameObject phone;
+    public Button button;
+
     Coroutine phone_co = null;
+    private bool isRunning = false;
 
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && phone_co != null)
+        //강제시청
+        /*
+                 if (Input.GetMouseButtonDown(0) && phone_co != null && isRunning)
         {
             StopCoroutine(phone_co);
-            phone.SetActive(false);
+            isRunning = false;
+            StartCoroutine(PhoneYpos(0f, -1100f));
+            button.enabled = true;
         }
+
+         */
     }
 
     public void startPhone()
     {
+        
+        isRunning = true;
         phone_co = StartCoroutine(Phone());
     }
 
 
     IEnumerator Phone()
     {
+        button.enabled = false;
+        isRunning = true;
 
         phone.SetActive(true);
 
@@ -38,7 +51,8 @@ public class Phone56 : MonoBehaviour
         yield return StartCoroutine(PhoneYpos(0f, -1100f));
 
         phone.SetActive(false);
-
+        isRunning = false;
+        button.enabled = true;
     }
 
     IEnumerator IBImsg()
@@ -47,11 +61,17 @@ public class Phone56 : MonoBehaviour
 
         for (int i = 0; i < msgs.Length; i++)
         {
+            if (!isRunning)
+            {
+                text.text = "";
+                yield break;
+            }
+
             text.text = msgs[i];
             yield return new WaitForSeconds(0.2f);
         }
 
-        yield return null;
+        yield return new WaitForSeconds(1f);
     }
 
     IEnumerator PhoneYpos(float start, float end)
@@ -62,10 +82,12 @@ public class Phone56 : MonoBehaviour
 
         while (Vector2.Distance(curPos, endPos) > 0.05f)
         {
-            curPos = Vector3.Lerp(curPos, endPos, 2f * Time.deltaTime);
+            curPos = Vector3.Lerp(curPos, endPos, 3f * Time.deltaTime);
             RECT.anchoredPosition = curPos;
 
              yield return null;
         }
+
+
     }
 }
