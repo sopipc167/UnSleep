13bdba4cb4754a3378dbe1f75ba8a3269ecd9cda
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-08-22 04:44:24 +0900

    큐브랑 별똥별에 Bloom (PostProcessing)

---
+++ UnSleep/Assets/Scripts/Mental_World/Camera/PostCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostCamera : MonoBehaviour
+{
+    private Camera mainCam;
+    private Camera postCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main.GetComponent<Camera>();
+        postCam = GetComponent<Camera>();
+    }
+
+
+    private void LateUpdate()
+    {
+        postCam.transform.position = mainCam.transform.position;
+        postCam.transform.rotation = mainCam.transform.rotation;
+
+    }
+}
