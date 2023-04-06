500a27236526e1e6ba4155d409c1e2a9484a555d
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2023-04-06 19:38:16 +0900

    클리어 처리 옮김

--- UnSleep/Assets/Scripts/ClockTower/Refactor/ClockLevelManager.cs
+++ UnSleep/Assets/Scripts/ClockTower/Refactor/ClockLevelManager.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClockLevelManager : MonoBehaviour
 {
-    public BCogWheel[] bCogWheels;
+    public PuzzleClear puzzleClear;
+    public Image blinkPanel;
+    public GameObject clearUI;
+
+    private BCogWheel[] bCogWheels;
+    private float clearCount = 0f;
+    private bool tiktoking = false;
+    private Coroutine coroutine;
+
 
     void Start()
     {
@@ -15,15 +24,90 @@ public class ClockLevelManager : MonoBehaviour
     {
         if (bCogWheels.Count(bcw => !bcw.satisfy) == 0)
         {
-            Debug.Log("클리어~");
+            clearCount += 1f * Time.deltaTime;
+            if (!tiktoking) 
+                coroutine = StartCoroutine(TicTok());
         } else
         {
+            clearCount = 0f;
+            if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+                coroutine = null;
+            }
+
         }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         checkClear();
+
+        if (clearCount >= 4f) clear();
+    }
+    public void clear()
+    {
+        if (Dialogue_Proceeder.instance.CurrentEpiID == 8 && !Dialogue_Proceeder.instance.AlreadyDone(81)) //23-1 클리어 시
+        {
+            Dialogue_Proceeder.instance.AddCompleteCondition(81);
+            clearUI.SetActive(true);
+            clearCount = 0f;
+        }
+        else if (Dialogue_Proceeder.instance.CurrentEpiID == 19 && Dialogue_Proceeder.instance.CurrentDiaID == 8036) //잘있어요-1 클리어 시
+        {
+            Dialogue_Proceeder.instance.AddCompleteCondition(31);
+            clearUI.SetActive(true);
+            // textManager.Set_Dialogue_Goodbye();
+            clearCount = 0f;
+        }
+        else if (Dialogue_Proceeder.instance.CurrentEpiID == 19 && Dialogue_Proceeder.instance.CurrentDiaID == 8037)
+        {
+            Dialogue_Proceeder.instance.AddCompleteCondition(32);
+            clearUI.SetActive(true);
+            // textManager.Set_Dialogue_Goodbye();
+            clearCount = 0f;
+        }
+        else
+        {
+            puzzleClear.ClearPuzzle(SceneType.Mental, 0f);
+            SoundManager.Instance.FadeOutBGM();
+        }
+    }
+
+    IEnumerator TicTok()
+    {
+
+        SoundManager.Instance.PlaySE("tictokshort");
+        float time = 0f;
+        tiktoking = true;
+
+        Color color = blinkPanel.color;
+        color.a = Mathf.Lerp(0f, 0.4f, time);
+
+        while (color.a < 0.4f)
+        {
+            time += Time.deltaTime / 0.5f;
+            color.a = Mathf.Lerp(0f, 0.4f, time);
+            blinkPanel.color = color;
+
+            yield return null;
+
+        }
+        time = 0f;
+
+        while (color.a > 0f)
+        {
+            time += Time.deltaTime / 0.5f;
+            color.a = Mathf.Lerp(0.4f, 0f, time);
+            blinkPanel.color = color;
+
+            yield return null;
+
+        }
+
+
+        tiktoking = false;
+        yield return null;
     }
 }
