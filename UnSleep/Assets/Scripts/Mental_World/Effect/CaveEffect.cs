d3d70da8fd9ae7893023e145c9a12089fdef1b53
Retrobas <ohminkwon2718@gmail.com>
2023-05-07 21:07:19 +0900

    Puzzle 별 Metal World Effect 추가
    5개 이벤트 대사 테이블에 추가 필요

---
+++ UnSleep/Assets/Scripts/Mental_World/Effect/CaveEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveEffect : MonoBehaviour, IEffect
+{
+    public AudioClip se19;
+    public AudioClip se20;
+    public AudioClip se27;
+    public AudioClip se50;
+    public AudioClip se65;
+
+    public void OnEffect()
+    {
+        switch (Dialogue_Proceeder.instance.CurrentEpiID)
+        {
+            case 19: SoundManager.Instance.PlaySE(se19); break;
+            case 20: SoundManager.Instance.PlaySE(se20); break;
+            case 27: SoundManager.Instance.PlaySE(se27); break;
+            case 50: SoundManager.Instance.PlaySE(se50); break;
+            case 65: SoundManager.Instance.PlaySE(se65); break;
+        }
+        
+    }
+}
---
+++ UnSleep/Assets/Scripts/Mental_World/Effect/ClockEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockEffect : MonoBehaviour, IEffect
+{
+    public OldCinemaEffect effect;
+    public ClockTowerObject towerObj;
+    public AudioClip ticSound;
+
+    public void OnEffect()
+    {
+        effect.enabled = true;
+        towerObj.OnEffect();
+        StartCoroutine(PlaySoundCoroutine());
+    }
+
+    private IEnumerator PlaySoundCoroutine()
+    {
+        while (true)
+        {
+            SoundManager.Instance.PlaySE(ticSound);
+            yield return new WaitForSeconds(Random.Range(0.8f, 1f));
+        }
+    }
+}
