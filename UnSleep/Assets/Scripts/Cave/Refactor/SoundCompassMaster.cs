fe3cad3389bd413507b97b7d5f913618bc2433dc
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2023-05-10 04:53:34 +0900

    (급) 동굴 소리나침반

---
+++ UnSleep/Assets/Scripts/Cave/Refactor/SoundCompassMaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundCompassMaster : MonoBehaviour
+{
+    public GameObject compass;
+
+    private void Start()
+    {
+        GetComponent<Toggle>().onValueChanged.AddListener(delegate { compassSetActive(); });
+    }
+
+    public void compassSetActive()
+    {
+        compass.SetActive(!compass.activeSelf);
+        // Debug.Log(compass.activeSelf);
+        // if (compass.activeSelf) compass.SetActive(false); else compass.SetActive(true);
+    }
+    
+}
