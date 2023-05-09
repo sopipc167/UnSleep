ed44da0c46bdd9f709176b1db9322defd7306c96
Retrobas <ohminkwon2718@gmail.com>
2023-05-10 07:33:55 +0900

    모든 오류 수정,,
    5시간 뒤 학교가야돼요

---
+++ UnSleep/Assets/Scripts/Mental_World/Camera/EnabledCube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnabledCube : MonoBehaviour
+{
+    public GameObject MWPuzzle;
+    private bool flag = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag) return;
+        if (!flag && MWPuzzle.activeSelf)
+        {
+            flag = true;
+            StartCoroutine(ActiveCoroutine()) ;
+        }
+    }
+
+    private IEnumerator ActiveCoroutine()
+    {
+        foreach (Transform item in transform)
+        {
+            item.gameObject.SetActive(false);
+        }
+        yield return new WaitUntil(() => !MWPuzzle.activeSelf);
+        foreach (Transform item in transform)
+        {
+            item.gameObject.SetActive(true);
+        }
+    }
+}
